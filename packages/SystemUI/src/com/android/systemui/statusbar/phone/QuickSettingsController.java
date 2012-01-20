5770bc3e0f10d7d241fd3d7644fe0909a8c46eee
Kasper Hettinga <kasper.hettinga@gmail.com>
2012-01-20 20:01:00 +0100

    add Fastcharge QS tile and PowerWidget (ported from Slim)
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Change-Id: Ia5e527467eedd98168d24f51d4cb596e63d19f3e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -18,7 +18,6 @@ package com.android.systemui.statusbar.phone;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -62,6 +61,7 @@ import com.android.systemui.quicksettings.UserTile;
 import com.android.systemui.quicksettings.WiFiDisplayTile;
 import com.android.systemui.quicksettings.WiFiTile;
 import com.android.systemui.quicksettings.WifiAPTile;
+import com.android.systemui.quicksettings.FChargeTile;
 
 public class QuickSettingsController {
     private static String TAG = "QuickSettingsController";
@@ -103,6 +103,7 @@ public class QuickSettingsController {
     public static final String TILE_PROFILE = "toggleProfile";
     public static final String TILE_NFC = "toggleNfc";
     public static final String TILE_USBTETHER = "toggleUsbTether";
+    public static final String TILE_FCHARGE = "toggleFCharge";
 
     private static final String TILE_DELIMITER = "|";
     private static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -156,7 +157,9 @@ public class QuickSettingsController {
     public static final int NFC_TILE = 21;
     public static final int SCREENTIMEOUT_TILE = 22;
     public static final int USBTETHER_TILE = 23;
+    public static final int FCHARGE_TILE = 24;
     public static final int USER_TILE = 99;
+
     private InputMethodTile IMETile;
 
     public QuickSettingsController(Context context, QuickSettingsContainerView container, PhoneStatusBar statusBarService) {
@@ -253,7 +256,9 @@ public class QuickSettingsController {
                 // Not available yet
             } else if (tile.equals(TILE_LTE)) {
                 // Not available yet
-            }
+            } else if (tile.equals(TILE_FCHARGE)) {
+                mQuickSettings.add(FCHARGE_TILE);
+			}
         }
 
         // Load the dynamic tiles
@@ -382,7 +387,6 @@ public class QuickSettingsController {
     void addQuickSettings(LayoutInflater inflater){
         // Load the user configured tiles
         loadTiles();
-
         // Now add the actual tiles from the loaded list
         for (Integer entry: mQuickSettings) {
             QuickSettingsTile qs = null;
@@ -462,6 +466,9 @@ public class QuickSettingsController {
                 break;
             case USBTETHER_TILE:
                 qs = new UsbTetherTile(mContext, inflater, mContainerView, this);
+                break;
+            case FCHARGE_TILE:
+                qs = new FChargeTile(mContext, inflater, mContainerView, this, mHandler);
                 break;
             }
             if (qs != null) {
