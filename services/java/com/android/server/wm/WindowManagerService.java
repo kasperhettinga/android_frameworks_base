a5371d1dbf34bcdcf1abbaa830d23387a06316d7
Timo Wendt <timo@tjwendt.de>
2012-08-30 12:18:41 +0300

    Framework: Development setting to enable navbar (1 of 2)
    This adds the framework support for enabling the Navigation bar
    on devices with hardware keys.  Intended for development use.
    
    Change-Id: I23a508fa3268e63fbbc9a8305d011db3ca7722a0

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -9940,6 +9940,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public boolean hasMenuKeyEnabled() {
         return mPolicy.hasMenuKeyEnabled();
     }
